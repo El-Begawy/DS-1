f8d23bc008d28962f4a1dc8f442eb3448958cac0
El-Begawy <45739074+El-Begawy@users.noreply.github.com>
2019-04-17 23:51:31 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs49_cs69;
+ interface ILinkedList {
+    void add(int index, Object element);
+    void add(Object element);
+    Object get(int index);
+    void set(int index, Object element);
+    void clear();
+    boolean isEmpty();
+    int size();
+    ILinkedList sublist(int fromIndex, int toIndex);
+    boolean contains(Object o);
+}
+
