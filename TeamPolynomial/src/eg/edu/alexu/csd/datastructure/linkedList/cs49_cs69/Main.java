225ca748c202d2c6025f81933ffe98c6eec17be2
aymanm419 <40718764+aymanm419@users.noreply.github.com>
2019-04-18 03:22:24 +0200

    Improved files & added functions

---
+++ TeamPolynomial/src/eg/edu/alexu/csd/datastructure/linkedList/cs49_cs69/Main.java
@@ -0,0 +1,16 @@
+package eg.edu.alexu.csd.datastructure.linkedList.cs49_cs69;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+	    PolynomialSolver polySolver = new PolynomialSolver();
+        Scanner Sc = new Scanner(System.in);
+	    String inp = Sc.nextLine();
+	    int tmp[][] = polySolver.StringTokens(inp);
+	    if(tmp == null)
+	        return;
+	    for(int i = 0;i < tmp.length;i++)
+	        System.out.println(tmp[i][0] + " " + tmp[i][1]);
+    }
+}
